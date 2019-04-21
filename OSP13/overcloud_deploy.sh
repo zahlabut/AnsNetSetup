91d62b48a5fd83c7438b1281ecc59038481d8f13
Arkady Shtempler <ashtempl@redhat.com>
2019-04-21 11:28:05 +0300

    changes in deplpy script

--- OSP13/overcloud_deploy.sh
+++ OSP13/overcloud_deploy.sh
@@ -7,11 +7,12 @@ openstack overcloud deploy \
 -e /home/stack/docker-images.yaml \
 -e /home/stack/templates/ironic-inspector.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /home/stack/templates/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/ironic.yaml \
 -e /home/stack/templates/ironic.yaml \
 -e /home/stack/templates/ceph.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ml2-ansible.yaml \
+#-e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ml2-ansible.yaml \
 -e /home/stack/templates/neutron-ml2-ansible.yaml \
---log-file overcloud_deployment.log
+--log-file overcloud_deployment.log \
+-v
