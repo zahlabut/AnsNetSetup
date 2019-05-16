0d46e006a829f8328505cc86538aa77ad2ae7d16
Arkady Shtempler <ashtempl@redhat.com>
2019-05-16 11:19:52 +0300

    add osp14 templates

---
+++ OSP14/overcloud_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+openstack overcloud update \
+--timeout 180 \
+--templates /usr/share/openstack-tripleo-heat-templates \
+--stack overcloud \
+--libvirt-type kvm \
+--ntp-server clock.redhat.com \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/templates/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/ironic.yaml \
+-e /home/stack/templates/ironic.yaml \
+-e /home/stack/templates/ceph.yaml \
+-e /home/stack/templates/nodes_data.yaml \
+-e /home/stack/containers-prepare-parameter.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ml2-ansible.yaml \
+--log-file overcloud_deployment.log \
+-v
